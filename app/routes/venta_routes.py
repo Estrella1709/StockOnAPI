6f9e43f99ad796ffb46cb4a9179820f6cbd4da91
Estrella1709 <estrellatorrijos17@gmail.com>
2025-07-06 19:39:13 -0600

    Endpoint - mostrar ventas

--- app/routes/venta_routes.py
+++ app/routes/venta_routes.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.schemas.venta_schema import VentaCreate, VentaOut
+from app.schemas import venta_schema
+from app.models import models
 from app.crud import venta_crud
 
 router = APIRouter(
@@ -12,3 +14,8 @@ router = APIRouter(
 @router.post("/", response_model=VentaOut)
 def registrar_venta(venta: VentaCreate, db: Session = Depends(get_db)):
     return venta_crud.crear_venta(db, venta)
+
+@router.get("/ventas/", response_model=list[venta_schema.VentaOut])
+def obtener_ventas(db: Session = Depends(get_db)):
+    ventas = db.query(models.Venta).all()
+    return ventas
