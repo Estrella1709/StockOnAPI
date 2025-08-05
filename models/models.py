77edfdf4c173a6942c5091b113d7c0382f42a418
Mario Rodarte <mariohrodarte@outlook.com>
2025-08-04 20:05:54 -0600

    Corrección de la base de datos

--- models/models.py
+++ models/models.py
@@ -27,6 +27,7 @@ class Producto(Base):
     precioUnitario = Column(DECIMAL(10, 2))
     dimensiones = Column(Text)
     peso = Column(DECIMAL(10, 2))
+    precauciones = Column(Text)
     cantidad = Column(Integer)
     caracteristicas = Column(Text)
     codigoBarras = Column(String(200))
@@ -47,7 +48,7 @@ class Inventario(Base):
 class TipoInventario(Base):
     __tablename__ = "tipo_inventarios"
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    nombre = Column(String, unique=True, nullable=False)
+    nombre = Column(String(255), unique=True, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow)
 
@@ -136,15 +137,15 @@ class Admin(Base):
 class Proveedor(Base):
     __tablename__ = 'proveedores'
     id = Column(Integer, primary_key=True, index=True)
-    nombre = Column(String, nullable=False)
-    correo = Column(String, nullable=False)
-    num_telefono = Column(String)
-    tipo_producto = Column(String)
-    condiciones_pago = Column(String)
-    frecuencia_suministro = Column(String)
-    horario_atencion = Column(String)
-    pais = Column(String)
-    ciudad = Column(String)
+    nombre = Column(String(100), nullable=False)
+    correo = Column(String(100), nullable=False)
+    numTelefono = Column(String(20))
+    tiposProducto = Column(String(255))
+    condicionesPago = Column(String(255))
+    frecuenciaSuministro = Column(String(255))
+    horarioAtencion = Column(String(255))
+    pais = Column(String(100))
+    ciudad = Column(String(100))
     id_empresa = Column(BigInteger, ForeignKey('empresa.id'))
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
